caddbb6e59ee8abe795c098130f80f55faceb5b6
Justin Caratzas <bigjust@lambdaphil.es>
2016-09-29 01:55:01 -0400

    fix testinfra lint warnings

--- tests/test_default.py
+++ tests/test_default.py
@@ -1,11 +1,9 @@
-from testinfra.modules import File, Service
-
-
 def test_nginx_service(Service):
     nginx_service = Service('nginx')
 
     assert nginx_service.is_running
     assert nginx_service.is_enabled
+
 
 def test_nginx_conf(File):
     nginx_conf = File('/etc/nginx/nginx.conf')
