7467947562f401ba5847592e0dc2fec17d426cff
Justin Caratzas <bigjust@lambdaphil.es>
2018-09-30 18:14:37 -0400

    use new style host.service in molecule tests

--- molecule/pagespeed/tests/test_default.py
+++ molecule/pagespeed/tests/test_default.py
@@ -6,15 +6,15 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_nginx_service(Service):
-    nginx_service = Service('nginx')
+def test_nginx_service(host):
+    nginx_service = host.service('nginx')
 
     assert nginx_service.is_running
     assert nginx_service.is_enabled
 
 
-def test_nginx_conf(File):
-    nginx_conf = File('/etc/nginx/nginx.conf')
+def test_nginx_conf(host):
+    nginx_conf = host.file('/etc/nginx/nginx.conf')
 
     assert nginx_conf.exists
     assert nginx_conf.group == 'root'
